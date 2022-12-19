acc88ca81a671665ada400189602eaeb972e3198
Caren-Koroeny <carensiya5@gmail.com>
2022-12-19 17:33:32 +0300

    fixed js linters errors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".hamburgur");
-const navsub = document.querySelector(".nav-sub");
+const hamburger = document.querySelector('.hamburgur');
+const navsub = document.querySelector('.nav-sub');
 hamburger.addEventListener('click', () => {
-hamburger.classList.toggle("change")
-navsub.classList.toggle("nav-change")
-})
+  hamburger.classList.toggle('change');
+  navsub.classList.toggle('nav-change');
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener('click', () => {
-        hamburger.classList.remove("change");
-        navsub.classList.remove("nav-change");
-    }))
+document.querySelectorAll('.nav-link').forEach((n) => n
+  .addEventListener('click', () => {
+    hamburger.classList.remove('change');
+    navsub.classList.remove('nav-change');
+  }));
