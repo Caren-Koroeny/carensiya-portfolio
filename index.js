cc015c82f1f4c853b1ad90ebce812f071505cd0f
shaaibu <shaaibusuleiman9@gmail.com>
2022-12-19 01:54:13 -0800

    Change javascript file

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const ham = document.querySelector(".hamburgur");
+const hamburger = document.querySelector(".hamburgur");
 const navsub = document.querySelector(".nav-sub");
-ham.addEventListener('click', () => {
-ham.classList.toggle("change")
+hamburger.addEventListener('click', () => {
+hamburger.classList.toggle("change")
 navsub.classList.toggle("nav-change")
 });
-
-// document.querySelectorAll(".nav-link").forEach(n  => n.
-//     addEventListener("click", () => {
-//         ham.classList.remove('change');
-//         navsub.classList.remove('nav-change');
-//     }))
