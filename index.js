594dab53ed1dcb1880001be91aca9e1be23f562b
Caren-Koroeny <carensiya5@gmail.com>
2022-12-19 14:09:34 +0300

    made changes on js when a link is clicked

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const navsub = document.querySelector(".nav-sub");
 hamburger.addEventListener('click', () => {
 hamburger.classList.toggle("change")
 navsub.classList.toggle("nav-change")
-});
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener('click', () => {
+        hamburger.classList.remove("change");
+        navsub.classList.remove("nav-change");
+    }))
