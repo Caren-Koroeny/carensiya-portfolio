32686bdff4a1de788fbe8a8bb97b90aeb6059d4c
Caren-Koroeny <carensiya5@gmail.com>
2022-12-23 15:09:01 +0300

    fixed js errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,24 @@
-function modalPopupFunc(){
-    const modalBtn = document.querySelector('.modal-btn');
-    const modalBg = document.querySelector('.modal-bg');
-    const modalClose = document.querySelector('.close');
-    modalBg.classList.add('bg-active');
-    modalClose.addEventListener('click', () => {
-        modalBg.classList.remove('bg-active')
-    })
-}
-
-
 const data = {
-    title1 : 'Tonic',
-    badges: ["Canopy", "images/counter-images.png", "Back End Dev", 'images/counter-images.png', 2015],
-    backgroundImage1: ["images/ModalPopup.svg","images/mobile-snapshot.png" ],
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-    icons: ['html', 'CSS', 'Bootstrap', 'Github','Rubby', 'JavaScript'],
-    mobileicons: ['html', 'css', 'javascript'],
-    links: ['See live','images/seelive.png', 'Github', "images/github.png"],
-}
+  title1: 'Tonic',
+  badges: ['Canopy', 'images/counter-images.png', 'Back End Dev', 'images/counter-images.png', 2015],
+  backgroundImage1: ['images/ModalPopup.svg', 'images/mobile-snapshot.png'],
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  icons: ['html', 'CSS', 'Bootstrap', 'Github', 'Rubby', 'JavaScript'],
+  mobileicons: ['html', 'css', 'javascript'],
+  links: ['See live', 'images/seelive.png', 'Github', 'images/github.png'],
+};
 
+const badgesContainer = document.querySelector('.card1-badges');
+badgesContainer.innerHTML += `
+  <li class="badges1">${data.badges[0]}</li>
+  <img src="${data.badges[1]}" class="modal-counter">
+  <li class="badges1">${data.badges[2]}</li>
+  <img src="${data.badges[3]}" class="modal-counter">
+  <li class="badges1">${data.badges[4]}</li>
+  
+`;
 
-const badgesContainer = document.querySelector('.card1-badges')
-data.badges.map((item, key) => {
-    if (key%2 !== 0){
-        badgesContainer.innerHTML += `
-        <img src="${item}" class="modal-counter">
-        `
-    }else {
-        badgesContainer.innerHTML += `
-        <li class="badges1">${item}</li>
-        `
-    }
-})
-
-const modallinks = document.querySelector(".link-button")
+const modallinks = document.querySelector('.link-button');
 
 modallinks.innerHTML += `
     <button class="livebutton">
@@ -44,53 +29,51 @@ modallinks.innerHTML += `
         ${data.links[2]}
         <img src="${data.links[3]}">
     </button>
-`
- 
-const header = document.querySelector(".title1tonic")
-header.innerHTML += data.title1
+`;
 
-const description1 = document.querySelector(".text-para")
-description1.innerHTML += 
-`
+const header = document.querySelector('.title1tonic');
+header.innerHTML += data.title1;
+
+const description1 = document.querySelector('.text-para');
+description1.innerHTML
++= `
 <p class="modal-description">${data.description}</p>
-`
-const modalimage1 = document.querySelector(".image1modal")
+`;
+const modalimage1 = document.querySelector('.image1modal');
 modalimage1.innerHTML = `
-<img src=${data.backgroundImage1[0]} class="modal-bg1">`
+<img src=${data.backgroundImage1[0]} class="modal-bg1">`;
 
-const mobileimage1 = document.querySelector(".mobileimage")
+const mobileimage1 = document.querySelector('.mobileimage');
 mobileimage1.innerHTML = `
-<img src=${data.backgroundImage1[1]} class="modal-bg2">`
+<img src=${data.backgroundImage1[1]} class="modal-bg2">`;
 
-
-const modalIcon = document.querySelector('.modalsIcon')
+const modalIcon = document.querySelector('.modalsIcon');
 data.icons.forEach((icontem) => {
-    modalIcon.innerHTML += `
+  modalIcon.innerHTML += `
     <li class="modal-icons">${icontem}</li>`;
-})
+});
 
-const mobile = document.querySelector('.mobileIcons')
+const mobile = document.querySelector('.mobileIcons');
 data.mobileicons.forEach((mobitem) => {
-    mobile.innerHTML += `
+  mobile.innerHTML += `
     <li class="mobile-icons">${mobitem}</li>`;
-})
+});
 
-//Modal card one section
+// Modal card one section
 const desktopCardData = {
-    desktopIimages: [
-      "/images/desktop-image1.png",  "/images/desktop-image2.png",
-      "/images/desktop-image3.png", "/images/desktop-image4.png",
-    ], 
-    counterImg: "/images/counter-images.png",
-    mobileImages: ["/images/image-1.png","/images/image-3.png" ,'/images/image-4.png'],
-    cardTitle: ["Tonic", "Multi-Post Stories", "Facebook 360", "Uber Navigation"],
-    badges1: ["Canopy", "Back&nbspEnd&nbspDev", 2015],
-    badges2: ["FACEBOOK", "Full&nbspStack&nbspDev", 2015],
-    badges3: ["FACEBOOK", "Full&nbspStack&nbspDev", 2015],
-    badges4: ["Canopy", "Lead Developer", 2018],
-    languages: ["html", "css", "JavaScript", "Ruby on rails"], 
-}
-
+  desktopIimages: [
+    '/images/desktop-image1.png', '/images/desktop-image2.png',
+    '/images/desktop-image3.png', '/images/desktop-image4.png',
+  ],
+  counterImg: '/images/counter-images.png',
+  mobileImages: ['/images/image-1.png', '/images/image-3.png', '/images/image-4.png'],
+  cardTitle: ['Tonic', 'Multi-Post Stories', 'Facebook 360', 'Uber Navigation'],
+  badges1: ['Canopy', 'Back&nbspEnd&nbspDev', 2015],
+  badges2: ['FACEBOOK', 'Full&nbspStack&nbspDev', 2015],
+  badges3: ['FACEBOOK', 'Full&nbspStack&nbspDev', 2015],
+  badges4: ['Canopy', 'Lead Developer', 2018],
+  languages: ['html', 'css', 'JavaScript', 'Ruby on rails'],
+};
 
 const cardOne = document.querySelector('.one');
 
@@ -119,9 +102,9 @@ cardOne.innerHTML += `
             <li>${desktopCardData.languages[1]}</li>
             <li>${desktopCardData.languages[2]}</li>
         </ul>
-        <button class="submit-btn modal-btn" type="submit" onclick="modalPopupFunc()">See Project</button>
+        <button class="submit-btn modal-btn" type="submit">See Project</button>
     </div>
-`
+`;
 const cardTwo = document.querySelector('.two');
 cardTwo.innerHTML += `
 <img src="${desktopCardData.desktopIimages[1]}" alt="Multi-Post Stories project" class="image">
@@ -154,12 +137,12 @@ cardTwo.innerHTML += `
             <li>${desktopCardData.languages[1]}</li>
             <li>${desktopCardData.languages[2]}</li>
         </ul>
-        <button class="submit-btn modal-btn"  type="submit" onclick="modalPopupFunc()">See Project</button>
+        <button class="submit-btn modal-btn"  type="submit">See Project</button>
       </div>
 
       <img src="${desktopCardData.desktopIimages[1]}" alt="multi-post stories project" class="desktop-image2">
     </div>
-`
+`;
 
 const cardThree = document.querySelector('.three');
 cardThree.innerHTML += `
@@ -204,13 +187,13 @@ cardThree.innerHTML += `
   </ul>
     
     <div> 
-      <button class="submit-btn mobile modal-btn" type="submit" onclick="modalPopupFunc()" >See Project</button>
-      <button class="submit-btn desktop modal-btn" type="submit" onclick="modalPopupFunc()" >See Project</button>
+      <button class="submit-btn mobile modal-btn" type="submit" >See Project</button>
+      <button class="submit-btn desktop modal-btn" type="submit" >See Project</button>
       <div class="handcursor"><img src="./images/hand-cursor1.svg" alt="cursor hover"></div>
     </div>
 </div>
 
-`
+`;
 
 const cardFour = document.querySelector('.four');
 
@@ -251,9 +234,25 @@ cardFour.innerHTML += `
           <li>${desktopCardData.languages[2]}</li>
         </ul>
         
-        <button class="submit-btn modal-btn" type="submit" onclick="modalPopupFunc()">See Project</button>
+        <button class="submit-btn modal-btn" type="submit">See Project</button>
       </div>
       <img src="${desktopCardData.desktopIimages[3]}" alt="uber navigation project" class="desktop-image4">
     </div>
 
-`
+`;
+
+const modalPopupFunc = () => {
+  const modalBg = document.querySelector('.modal-bg');
+  const modalClose = document.querySelector('.close');
+  modalBg.classList.add('bg-active');
+  modalClose.addEventListener('click', () => {
+    modalBg.classList.remove('bg-active');
+  });
+};
+
+const modalsButton = document.querySelectorAll('.modal-btn');
+modalsButton.forEach((item) => {
+  item.addEventListener('click', () => {
+    modalPopupFunc();
+  });
+});
